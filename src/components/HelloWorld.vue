62f0bfea4f6524021ac94668bc95cf13307ea350
ericholdridge <eholdridge2@gmail.com>
2022-05-24 23:07:58 -0500

    changes made to the code under the new_branch branch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,9 @@ defineProps({
 		<h3>
 			You’ve successfully created a project with
 			<a target="_blank" href="https://vitejs.dev/">Vite</a> +
-			<a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+			<a target="_blank" href="https://vuejs.org/"
+				>Vue 3 new_branch changes</a
+			>.
 		</h3>
 	</div>
 </template>
